79e90d2a02ef4e65c895fc43c7b466fc4a336638
folkertm <folkert.meeuw@googlemail.com>
2020-07-17 19:25:40 +0200

    Update __init__.py
    (livetest3) [fmeeuw@meinherz ~]$ python3 mylivetest3.py 
    {0: <Form />}
    OrderedDict([('ie', [<Hidden name="ie">]), ('hl', [<Hidden name="hl">]), ('source', [<Hidden name="source">]), ('biw', [<Hidden name="biw">]), ('bih', [<Hidden name="bih">]), ('q', [<Text name="q">]), ('btnG', [<Submit name="btnG">]), ('btnI', [<Submit name="btnI" id="tsuid1">]), ('iflsig', [<Hidden name="iflsig">]), ('gbv', [<Hidden name="gbv" id="gbv">])])
    Traceback (most recent call last):
      File "mylivetest3.py", line 8, in <module>
        resp = form.submit()
      File "/home/fmeeuw/livetest3/lib/python3.7/site-packages/webtest/forms.py", line 631, in submit
        params=fields, **args)
      File "/home/fmeeuw/livetest3/src/livetest3/livetest3/__init__.py", line 130, in goto
        scheme, host, path, query, fragment = urllib.parse.urlsplit(href)
    NameError: name 'urllib' is not defined

--- livetest3/__init__.py
+++ livetest3/__init__.py
@@ -42,7 +42,7 @@ import sys
 import webtest
 import http.client
 from urllib.parse import urlparse
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urlunsplit, urljoin
 from http.cookies import BaseCookie, CookieError
 from http.cookiejar import CookieJar
 
@@ -127,11 +127,11 @@ def goto(self, href, method='get', **args):
     Monkeypatch the TestResponse.goto method so that it doesn't wipe out the
     scheme and host.
     """
-    scheme, host, path, query, fragment = urllib.parse.urlsplit(href)
+    scheme, host, path, query, fragment = urlsplit(href)
     # We
     fragment = ''
-    href = urllib.parse.urlunsplit((scheme, host, path, query, fragment))
-    href = urllib.parse.urljoin(self.request.url, href)
+    href = urlunsplit((scheme, host, path, query, fragment))
+    href = urljoin(self.request.url, href)
     method = method.lower()
     assert method in ('get', 'post'), (
         'Only "get" or "post" are allowed for method (you gave %r)'
